2b6475bba5c96906064ce3c1f88dee7b415f3240
Flavia Garbetta <flavia.garbetta@gmail.com>
2025-01-29 22:02:02 +0100

    Añado ejercicio 2

---
+++ 2.bugs.js
@@ -0,0 +1,15 @@
+// Ejercicio 2: arreglar bug
+
+const calcularPromedio = (numeros) => {
+    let sumaTotal = 0;
+    for (let i = 0; i < numeros.length; i++) {  
+        sumaTotal += numeros[i]; 
+    } 
+    let promedio = sumaTotal / numeros.length;
+    return promedio;
+};
+
+const listaNumeros = [1, 2, 3, 4, 5];
+const promedioNumeros = calcularPromedio(listaNumeros);
+
+console.log(promedioNumeros)
