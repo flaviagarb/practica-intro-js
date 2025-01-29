333ce9cf8b065fa88c022c4aa8333bbbaad052f1
Flavia Garbetta <flavia.garbetta@gmail.com>
2025-01-29 22:03:43 +0100

    Añado ejercicio 3.1

---
+++ 3.1.js
@@ -0,0 +1,43 @@
+// 3.1
+
+const input = [
+    "Downloads", 
+    "Videos",
+    "capture",
+    "mp4"
+];
+    
+const transform = (list0) => {
+    const pathList = list0.slice(0,list0.length - 1)
+
+    const path = pathList .join("/")
+
+    const ext = list0[list0.length - 1]
+
+    const result = path + "." + ext
+
+    return result
+};
+
+console.log(transform(input))
+
+
+const input2 = [
+    "CodinGame", 
+    "python",
+    "py"
+];
+
+console.log(transform(input2))
+
+const input3 = [
+    "programing", 
+    "languages",
+    "easy",
+    "beginner",
+    "useful", 
+    "pythonstuff",
+    "py"
+];
+
+console.log(transform(input3))
